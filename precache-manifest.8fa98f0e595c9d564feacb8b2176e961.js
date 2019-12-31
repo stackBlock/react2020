251459742da126cb1e731a50969388c9d2e71a5d
stackblock <lambdaTL.anthony@gmail.com>
2019-12-30 21:08:08 -0500

    Updates

---
+++ precache-manifest.8fa98f0e595c9d564feacb8b2176e961.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12fcdc4481ea6962232e36fbd9e1e0be",
+    "url": "/react2020/index.html"
+  },
+  {
+    "revision": "0e50a67c521bad3f4fe3",
+    "url": "/react2020/static/css/main.356ce7e9.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/react2020/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react2020/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0e50a67c521bad3f4fe3",
+    "url": "/react2020/static/js/main.91b51d2a.chunk.js"
+  },
+  {
+    "revision": "a8fd2c16abc13cc867ba",
+    "url": "/react2020/static/js/runtime-main.2a357624.js"
+  }
+]);
